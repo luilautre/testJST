4a50cf3b822806bdbcdb6cf9aa636e4b95bdee7e
luilautre <achillebou13@gmail.com>
2026-02-16 19:30:24 +0100

    Implement temporary debug route in server.js
    Added a debug route to expose root directory and public files.

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const { creerApp } = require('@luilautre/jst');
+const fs = require('fs');
+
+const racine = path.resolve('./');
 
 const app = creerApp({ 
-  racine: path.resolve('./'),
-  public: path.resolve('./public')
+  racine: racine,
+  public: path.join(racine, 'public')
+});
+
+// Route de debug temporaire
+app.get('/_debug', (req, res) => {
+  res.json({
+    racine: racine,
+    dirname: __dirname,
+    fichiers_racine: fs.readdirSync(racine),
+    fichiers_public: fs.existsSync(path.join(racine, 'public')) 
+      ? fs.readdirSync(path.join(racine, 'public')) 
+      : 'dossier public introuvable'
+  });
 });
 
 module.exports = app;
-
-if (require.main === module) {
-  app.listen(3000, () => console.log('Serveur sur http://localhost:3000'));
-}
