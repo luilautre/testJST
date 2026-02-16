17044c229a820b894ce003933ffbeb0cdca53ca2
luilautre <achillebou13@gmail.com>
2026-02-16 20:00:41 +0100

    Remove debug route from server.js
    Removed temporary debug route and cleaned up code.

--- server.js
+++ server.js
@@ -4,21 +4,9 @@ const fs = require('fs');
 
 const racine = path.resolve('./');
 
-const app = creerApp({ 
+const app = creerApp({
   racine: racine,
   public: path.join(racine, 'public')
-});
-
-// Route de debug temporaire
-app.get('/_debug', (req, res) => {
-  res.json({
-    racine: racine,
-    dirname: __dirname,
-    fichiers_racine: fs.readdirSync(racine),
-    fichiers_public: fs.existsSync(path.join(racine, 'public')) 
-      ? fs.readdirSync(path.join(racine, 'public')) 
-      : 'dossier public introuvable'
-  });
 });
 
 module.exports = app;
