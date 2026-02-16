b2cad4db171321b82b3b75657ab2ef33698b1eb9
luilautre <achillebou13@gmail.com>
2026-02-16 19:01:21 +0100

    Add local server listening for development
    Added local server listening configuration for development.

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const { creerApp } = require('@luilautre/jst');
 
 const app = creerApp({ racine: path.join(__dirname, 'public') });
 
+// Pour Vercel
 module.exports = app;
+
+// Pour local
+if (require.main === module) {
+  app.listen(3000, () => console.log('Serveur sur http://localhost:3000'));
+}
