a32f207a7af71d5ea04d41e09a0738c282cfef3c
luilautre <achillebou13@gmail.com>
2026-02-16 20:23:41 +0100

    Implement custom 404 error handling
    Add custom 404 error handling to serve index.html or a message.

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ const app = creerApp({
 });
 
 module.exports = app;
+
+app.use((req, res) => {
+  const chemin404 = path.join(path.resolve('./'), 'public', 'index.html');
+  if (fs.existsSync(chemin404)) {
+    res.status(404).send(fs.readFileSync(chemin404, 'utf8'));
+  } else {
+    res.status(404).send('404 — Page introuvable');
+  }
+});
